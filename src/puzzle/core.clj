f4f0b051acb62289c1e97d484c9c27141f9619c4
eu400000 <59219148+eu400000@users.noreply.github.com>
2022-12-01 08:57:33 +0100

    Initial commit

---
+++ src/puzzle/core.clj
@@ -0,0 +1,21 @@
+(ns puzzle.core
+  (:gen-class)
+  (:require [puzzle.support-functions :refer :all]))
+
+;; https://adventofcode.com/
+;; (c) Eric Uitbeijerse
+;; Dec 2021
+
+(defn puzzlePart1
+  [PuzzleData]
+)
+
+(defn puzzlePart2
+  [PuzzleData])
+
+
+(comment
+  (puzzlePart1 (loadData "resources/puzzleInput.txt"))
+
+  (puzzlePart2 (loadData "resources/puzzleInput.txt"))
+  )
