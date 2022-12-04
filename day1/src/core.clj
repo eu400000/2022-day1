d79040af94d97a7f5b0e4f79c92c562d16e6ef1a
eu400000 <59219148+eu400000@users.noreply.github.com>
2022-12-04 11:23:26 +0100

    Trying to get the new folder structure to work

--- day1/src/core.clj
+++ day1/src/core.clj
@@ -1,10 +1,33 @@
-(ns puzzle.core
+(ns day1.core
   (:gen-class)
-  (:require [puzzle.support-functions :refer :all]))
+  (:require [clojure.string :as str]))
 
 ;; https://adventofcode.com/
 ;; (c) Eric Uitbeijerse
 ;; Dec 2021
+
+
+(defn loadData
+  [inputFileName]
+  (slurp inputFileName))
+
+(defn process [data]
+  (let [split (str/split data #"\r\n")
+        convert (map #(Integer/parseInt %) split)]
+    convert))
+
+(defn parse-data [data]
+  (let [parse-by-elf (re-seq #"(?ms)[0-9\r\n]+?\r\n\r\n" (str data "\r\n\r\n"))
+        processed-elves (map #(process %1) parse-by-elf)]
+    processed-elves))
+
+
+(comment
+
+  (def d1 (str (slurp "input.txt") "\r\n\r\n"))
+  (parse-data d1))
+
+
 
 (defn puzzlePart1 [PuzzleData]
   (let [data (parse-data PuzzleData)
@@ -21,11 +44,11 @@
 
 (comment
   
-  (puzzlePart1 (loadData "resources/puzzleTestInput.txt"))
-  (puzzlePart1 (loadData "resources/puzzleInput.txt"))
+  (puzzlePart1 (loadData "day1/src/test-input.txt"))
+  (puzzlePart1 (loadData "day1/src/Input.txt"))
 
-  (puzzlePart2 (loadData "resources/puzzleTestInput.txt"))
-  (puzzlePart2 (loadData "resources/puzzleInput.txt"))
+  (puzzlePart2 (loadData "day1/src/test-input.txt"))
+  (puzzlePart2 (loadData "day1/src/input.txt"))
  
     
   )
